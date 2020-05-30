89bd251f160958b61014764fe6714df70bf4f8d0
Przemek Kapela <przmek.kapela92@gmail.com>
2020-05-30 13:08:15 +0200

    Create sign up header

---
+++ App/Controllers/Signup.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\View;
+
+/**
+ * Signup controller
+ *
+ * PHP version 7.0
+ */
+class Signup extends \Core\Controller
+{
+  /**
+   * Show the signup page
+   *
+   * @return void
+   */
+  public function newAction()
+  {
+      View::renderTemplate('Signup/new.html');
+  }
+}
+
+ ?>
