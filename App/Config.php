6c30e7859028294457b1fcfe32213ae6d239caee
Przemek Kapela <przmek.kapela92@gmail.com>
2020-06-10 11:08:13 +0200

    registration and login

--- App/Config.php
+++ App/Config.php
@@ -20,23 +20,29 @@ class Config
      * Database name
      * @var string
      */
-    const DB_NAME = 'mvc-budget';
+    const DB_NAME = 'przemy10_mvc-mybudget';
 
     /**
      * Database user
      * @var string
      */
-    const DB_USER = 'root';
+    const DB_USER = 'przemy10_kapela';
 
     /**
      * Database password
      * @var string
      */
-    const DB_PASSWORD = '';
+    const DB_PASSWORD = 'przemekkapela11';
 
     /**
      * Show or hide error messages on screen
      * @var boolean
      */
     const SHOW_ERRORS = true;
+
+    const SECRET_KEY = 'B3FA81524658816F61B1C822E94B1';
+
+    const SECRET_MAIL_GRID ='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';
+
+
 }
